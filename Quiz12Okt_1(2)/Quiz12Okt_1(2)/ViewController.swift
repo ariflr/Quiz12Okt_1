02f6c15f0a789d89f9a13820709e115deb0e87ec
ariflr <32724960+ariflr@users.noreply.github.com>
2017-10-12 09:58:29 +0700

    Create Xcode Project

---
+++ Quiz12Okt_1(2)/Quiz12Okt_1(2)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz12Okt_1(2)
+//
+//  Created by arif luqman rabono on 10/12/17.
+//  Copyright © 2017 arif luqman rabono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
